e369a334931f55945faad70e7144476bde97e628
yuyu <Amy_Wei@corpnet.asus>
2023-02-04 17:47:27 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: "w5-vite-w5-vue-vite-team/",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
